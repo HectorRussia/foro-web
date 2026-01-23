d05c9c47d8ed43b79fa1d1f0ff1e17afbe3f7707
Ponkrit <ponkrit.wo@hotmail.com>
2026-01-23 16:40:57 +0700

    feat: component protect route

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
+      {/* Public Routes*/}
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+
+      {/* Protected Routes*/}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/*add more route*/}
+      </Route>
+
     </Routes>
   );
 }
