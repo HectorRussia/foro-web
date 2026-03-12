c27051fab5487b23e09ea8675e6107bf17bafb55
Ponkrit@techcombine <ponkrit.w@techcombine.co>
2026-03-13 00:50:57 +0700

    chore: update vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -13,45 +13,49 @@ import RealtimeSearch from './pages/RealtimeSearch';
 import TodayNews from './pages/TodayNews';
 import LandingPage from './pages/LandingPage';
 import PublicRoute from './components/auth/PublicRoute';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
-    <Routes>
-      {/* Public Routes - Accessible by everyone */}
-      <Route path="/" element={<LandingPage />} />
+    <>
+      <Routes>
+        {/* Public Routes - Accessible by everyone */}
+        <Route path="/" element={<LandingPage />} />
 
-      {/* Guest-only Routes - Redirect to dashboard if already logged in */}
-      <Route element={<PublicRoute />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Route>
-
-      {/* Protected Routes*/}
-      <Route element={<ProtectedRoute />}>
-        <Route path="/today-news" element={<TodayNews />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/user-target" element={<UserTarget />} />
-        <Route path="/user-following" element={<UserFollow />} />
-        <Route path="/category-management" element={<Category />} />
-        <Route path="/category-news/:id" element={<CategoryNews />} />
-
-        {/*add more route*/}
-
-        {/*For King*/}
-        <Route element={<ProtectedRoute requiredRole={"king"} />}>
-          <Route path="/advance-search" element={<AdvanceSearch />} />
-          <Route path="/realtime-search" element={<RealtimeSearch />} />
-          <Route path="/today-news" element={<TodayNews />} />
-        </Route>
-        {/*For Queen*/}
-        <Route element={<ProtectedRoute requiredRole={"queen"} />}>
-          <Route path="/realtime-search" element={<RealtimeSearch />} />
-          <Route path="/today-news" element={<TodayNews />} />
+        {/* Guest-only Routes - Redirect to dashboard if already logged in */}
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
 
-      </Route>
+        {/* Protected Routes*/}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/today-news" element={<TodayNews />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/user-target" element={<UserTarget />} />
+          <Route path="/user-following" element={<UserFollow />} />
+          <Route path="/category-management" element={<Category />} />
+          <Route path="/category-news/:id" element={<CategoryNews />} />
 
-    </Routes>
+          {/*add more route*/}
+
+          {/*For King*/}
+          <Route element={<ProtectedRoute requiredRole={"king"} />}>
+            <Route path="/advance-search" element={<AdvanceSearch />} />
+            <Route path="/realtime-search" element={<RealtimeSearch />} />
+            <Route path="/today-news" element={<TodayNews />} />
+          </Route>
+          {/*For Queen*/}
+          <Route element={<ProtectedRoute requiredRole={"queen"} />}>
+            <Route path="/realtime-search" element={<RealtimeSearch />} />
+            <Route path="/today-news" element={<TodayNews />} />
+          </Route>
+
+        </Route>
+
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
